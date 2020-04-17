95436896b592c9ca069b79da696f739221999599
Mike Houser <saintmikel2004@gmail.com>
2020-04-17 15:01:25 -0400

    more code

--- index.js
+++ index.js
@@ -78,4 +78,130 @@ class App {
         employeeInfo.officeNumber = await managerInfo.officeNumber;
 
         return employeeInfo;
+}
+
+async getGithubHandle(employeeInfo) {
+    let engineerInfo =
+        await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "GitHub handle: ",
+                    name: "github"
+                }
+            ]);
+
+    employeeInfo.github = await engineerInfo.github;
+
+    return employeeInfo;
+}
+
+async getSchoolInfo(employeeInfo) {
+    let internInfo =
+        await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "School: ",
+                    name: "school"
+                }
+            ]);
+
+    employeeInfo.school = internInfo.school;
+
+    return employeeInfo;
+}
+
+createEmployee(employeeInfo) {
+    let employee;
+    const { id, name, email } = employeeInfo;
+    switch (employeeInfo.title.toLowerCase()) {
+        case 'manager':
+            const manager = new Manager(name, id, email, employeeInfo.officeNumber);
+            employee = manager;
+            break;
+        case 'engineer':
+            const engineer = new Engineer(name, id, email, employeeInfo.github);
+            employee = engineer;
+            break;
+        case 'intern':
+            const intern = new Intern(name, id, email, employeeInfo.school);
+            employee = intern;
+            break;
+        default:
+            break;
     }
+
+    return employee;
+}
+
+saveEmployeeToDb(employee) {
+    switch (employee.getRole().toLowerCase()) {
+        case 'manager':
+            this.db.manager = employee;
+            break;
+        case 'engineer':
+            this.db.engineers.push(employee);
+            break;
+        case 'intern':
+            this.db.interns.push(employee);
+            break;
+        default:
+            break;
+    }
+}
+
+createTeamRoster() {
+
+    let managerProfile = '';
+    let engineers = '';
+    let interns = '';
+
+    if (this.db.manager) {
+        managerProfile = new ManagerProfile(this.db.manager);
+        managerProfile = managerProfile.createProfile();
+    }
+
+    if (this.db.engineers) {
+        for (const engineer of this.db.engineers) {
+            let engineerProfile = new EngineerProfile(engineer);
+            engineerProfile = engineerProfile.createProfile();
+
+            engineers += engineerProfile;
+        }
+    }
+
+    if (this.db.interns) {
+        for (const intern of this.db.interns) {
+            let internProfile = new InternProfile(intern);
+            internProfile = internProfile.createProfile();
+
+            engineers += internProfile;
+        }
+    }
+
+    const team = managerProfile + engineers + interns;
+
+    let teamRoster = new TeamRoster(team);
+    teamRoster = teamRoster.createTeamRoster();
+
+    return teamRoster;
+}
+
+createServer(teamRoster) {
+
+    fs.writeFile('./public/team.html', teamRoster, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+
+    http.createServer(function (req, res) {
+        fs.readFile('./public/team.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+
+    }).listen(8080);
+}}
